b1d6923a64aaef0649bb7b9d232244ef40f07d50
Sridharn97 <sridhar.n2023cse@sece.ac.in>
2026-07-01 15:11:48 +0530

    Conditions Applied

--- Product Inventory Entity Manager/Main.java
+++ Product Inventory Entity Manager/Main.java
@@ -1,33 +1,25 @@
 import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter product id: ");
+        System.out.print("Enter product ID: ");
         String productId = sc.nextLine();
-
         System.out.print("Enter product name: ");
         String productName = sc.nextLine();
-
-        System.out.print("Enter price: ");
+        System.out.print("Enter product price: ");
         double price = sc.nextDouble();
-
         System.out.print("Enter stock quantity: ");
         int stockQuantity = sc.nextInt();
-
         System.out.print("Enter discount percentage: ");
         double discount = sc.nextDouble();
-
         Product p = new Product(productId, productName, price, stockQuantity);
         p.applyDiscount(discount);
-
-        System.out.println();
         System.out.println("Product Details:");
-        System.out.println("ID: " + p.getProductId());
-        System.out.println("Name: " + p.getProductName());
-        System.out.printf("Price: %.2f%n", p.getPrice());
+        System.out.println("Product ID: " + p.getProductId());
+        System.out.println("Product Name: " + p.getProductName());
+        System.out.println("Price after discount: " + p.getPrice());
         System.out.println("Stock Quantity: " + p.getStockQuantity());
-
         sc.close();
     }
 }
--- Product Inventory Entity Manager/Product.java
+++ Product Inventory Entity Manager/Product.java
@@ -7,60 +7,58 @@ public class Product {
     public Product(String productId, String productName, double price, int stockQuantity) {
         this.productId = productId;
         this.productName = productName;
-
-        if (price < 0)
-            this.price = 0.0;
-        else
-            this.price = price;
-
-        if (stockQuantity < 0)
-            this.stockQuantity = 0;
-        else
-            this.stockQuantity = stockQuantity;
+        setPrice(price);
+        setStockQuantity(stockQuantity);
     }
 
     public String getProductId() {
         return productId;
     }
 
-    public String getProductName() {
-        return productName;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public int getStockQuantity() {
-        return stockQuantity;
-    }
-
-    // Setters
     public void setProductId(String productId) {
         this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
     }
 
     public void setProductName(String productName) {
         this.productName = productName;
     }
 
+    public double getPrice() {
+        return price;
+    }
+
     public void setPrice(double price) {
-        if (price < 0)
+        if (price < 0) {
             this.price = 0.0;
-        else
+        } else {
             this.price = price;
+        }
+    }
+    public int getStockQuantity() {
+        return stockQuantity;
     }
 
     public void setStockQuantity(int stockQuantity) {
-        if (stockQuantity < 0)
+        if (stockQuantity < 0) {
             this.stockQuantity = 0;
-        else
+        } else {
             this.stockQuantity = stockQuantity;
+        }
     }
 
     public void applyDiscount(double percentage) {
-        if (percentage >= 0 && percentage <= 100) {
-            price = price - (price * percentage / 100);
+        if (percentage < 0) {
+            return;
+        }
+
+        price = price - (price * percentage / 100.0);
+
+        if (price < 0) {
+            price = 0.0;
         }
     }
 }
